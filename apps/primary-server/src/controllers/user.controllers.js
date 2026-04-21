f20fbf506237608e3c9de136d883f50bb1999615
Hirdesh Keswani <hirdeshkeshwani2001@gmail.com>
2026-04-21 22:03:47 +0530

    added user related controllers and services without logic

--- apps/primary-server/src/controllers/user.controllers.js
+++ apps/primary-server/src/controllers/user.controllers.js
@@ -1,3 +1,28 @@
-export async function handleCreateUser(req,res){}
-export async function handleGetUserProfile(req,res){}
-export async function handleDeleteUserProfile(req,res){}
+import {loginUser,logoutUser,createNewuser,getUserProfile,deleteUser} from "../services/user.services.js";
+
+
+export async function handleCreateUser(request,response){
+    try{
+        const {fullname,username,email,password} = request.body;
+        if(!fullname || !username || !email || password){
+            return response.status(400).json({message:"Full Name or username or email or Password"});
+        }
+
+        const newUser = await userService.createUser(fullname,username,email,password);
+
+        return response.status(200).json({message:"New User Created success",data:newUser});
+    }
+    catch(error){
+        console.log("Internal Server Error");
+        return response.status(500).json({message:"Internal Server error",data:null});
+    }
+}
+
+export async function handleUserLogin(request,response){}
+
+export async function handleUserLogout(request,response){}
+
+export async function handleGetUserProfile(request,response){}
+
+export async function handleDeleteUserProfile(request,response){}
+
---
+++ apps/primary-server/src/services/user.services.js
@@ -0,0 +1,26 @@
+    
+  export async function createNewuser(fullname,username,email,password) {
+    
+    // step-1 check for email and username exists
+
+    // step-2 if no entry exist create in database
+    
+  } 
+  
+  export async function getUserProfile(){
+    // step-1 check if user exist in db
+
+    // if yes fetch user info and return
+  }
+
+
+  export async function deleteUser() {
+    // step-1 get user and delete
+  }
+
+  export async function loginUser(){}
+
+
+  export async function logoutUser(){}
+
+
